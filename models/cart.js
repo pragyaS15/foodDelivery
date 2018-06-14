213f59f2749529f2a06c2db23f18e843c9432fb5
pragyaS15 <40213179+pragyaS15@users.noreply.github.com>
2018-06-14 07:47:18 +0530

    Add files via upload

---
+++ models/cart.js
@@ -0,0 +1,35 @@
+var Promise = require('bluebird');
+var mongoose = Promise.promisifyAll(require('mongoose'));
+
+var bcrypt = Promise.promisifyAll(require('bcrypt'));
+
+var Schema = mongoose.Schema;
+
+var Cart = new Schema({
+	email: {
+		type: String
+	},
+	modifiedOn: {
+		type: Date
+	},
+	quantity: {
+		type: Date
+	},
+	price: {
+		type: String
+	},
+	category: {
+		type: String
+	},
+	ingredients: {
+		type: String
+	},
+	availability: {
+		type: String
+	},
+	image: {
+		type: String
+	}
+}, { versionKey: false });
+
+module.exports = mongoose.model('Cart', Cart);
